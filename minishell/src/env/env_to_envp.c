b769b4aa51382a0eb70d4be2555de532d6748634
Caio <caio.cntct@gmail.com>
2025-11-13 16:35:17 +0100

    Env_to_envp

---
+++ minishell/src/env/env_to_envp.c
@@ -0,0 +1,57 @@
+#include "../include/minishell.h"
+
+static int	env_count(t_env *env)
+{
+	int count;
+
+	count = 0;
+	while (env)
+	{
+		count++;
+		env = env->next;
+	}
+	return (count);
+}
+
+void	free_envp(char **envp)
+{
+	int i;
+
+	if (!envp)
+		return ;
+	i = 0;
+	while (envp[i])
+	{
+		free(envp[i])
+		i++;
+	}
+	free(envp);
+}
+
+char	**env_to_envp(t_env *env)
+{
+	char	**envp;
+	char	*tmp;
+	int		size;
+	int		i;
+
+	size = env_count(env);
+	envp = malloc((size + 1) * sizeof(char *));
+	if (!envp)
+		return (NULL);
+	i = 0;
+	while (env)
+	{
+		tmp = ft_strjoin(env->key, "=");
+		if (!tmp)
+			return (free_envp(envp), NULL);
+		envp[i] = ft_strjoin(tmp, env->val);
+		free(tmp);
+		if (!envp[i])
+			return (free_envp(envp), NULL);
+		env = env->next;
+		i++;
+	}
+	envp[i] = NULL;
+	return (envp);
+}
