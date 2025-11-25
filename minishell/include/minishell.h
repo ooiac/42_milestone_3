ea0151b330df2cc8f2e166220ca973eb67701279
Caio Eduardo Da Silva <caida-si@c1r8s4.42luxembourg.lu>
2025-11-25 20:20:13 +0100

    minishell.h voltou p branch

---
+++ minishell/include/minishell.h
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caida-si <caida-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 17:44:30 by caida-si          #+#    #+#             */
+/*   Updated: 2025/11/20 15:21:53 by caida-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+
+# include "../libft/libft.h"
+
+typedef struct	s_env
+{
+	char			*key;
+	char			*val;
+	struct s_env	*next;
+}	t_env;
+
+/* env */
+t_env	*env_from_envp(char **envp);
+void	free_env(t_env *env);
+char	*env_get(t_env *env, const char *key);
+char	**env_to_envp(t_env *env);
+void	free_envp(char **envp);
+
+/* exec */
+char	*resolve_executable(const char *cmd, t_env *env);
+int		run_single_command(char **av, t_env *env);
+int		run_pipeline(char ***cmds, t_env *env);
+
+/* pipeline helpers */
+int		**create_pipes(int n);
+void	close_all_pipes(int **pipes, int n);
+void	exec_in_pipeline(char **cmd, int **pipes, int i, int n_cmds,
+						t_env *env);
+int		wait_all(pid_t *pids, int n);
+
+/* builtins */
+int		is_builtin(const char *cmd);
+int		exec_builtin(char **args, t_env **env);
+int		builtin_echo(char **args);
+int		builtin_pwd(void);
+int		builtin_env(t_env *env);
+int		builtin_cd(char **args, t_env **env);
+int		builtin_export(char **args, t_env **env);
+int		builtin_unset(char **args, t_env **env);
+int		builtin_exit(char **args);
+
+/* main loop */
+void	start_shell(t_env *env);
+
+#endif
