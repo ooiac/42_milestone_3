7c8656887ad4a196092354fa4fcf961bad70cfb1
Caio Eduardo Da Silva <caida-si@c1r8s4.42luxembourg.lu>
2025-11-25 20:31:53 +0100

    versao que compila adicionada. ajustes ainda pendentes

--- minishell/main.c
+++ minishell/main.c
@@ -6,11 +6,13 @@
 /*   By: caida-si <caida-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 18:10:43 by caida-si          #+#    #+#             */
-/*   Updated: 2025/11/10 18:46:35 by caida-si         ###   ########.fr       */
+/*   Updated: 2025/11/25 20:31:26 by caida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+extern volatile sig_atomic_t g_signal_received;
 
 int	main(int ac, char **av, char **envp)
 {
